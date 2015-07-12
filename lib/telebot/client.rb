b98a5b694dbd0faffcd751410e68e784c7280645
Potapov Sergey <blake131313@gmail.com>
2015-07-12 23:55:43 +0200

    Add forward message call

--- lib/telebot/client.rb
+++ lib/telebot/client.rb
@@ -17,7 +17,7 @@
 
 module Telebot
   class Client
-    API_URL = "https://api.telegram.org"
+    API_URL = "https://api.telegram.org".freeze
 
     def initialize(token, adapter: :net_http)
       @token = token
@@ -39,12 +39,40 @@ module Telebot
       User.new(result)
     end
 
+    # Use this method to receive incoming updates using long polling.
+    # An Array of Update objects is returned.
+    #
+    # Note:
+    # 1. This method will not work if an outgoing webhook is set up.
+    # 2. In order to avoid getting duplicate updates, recalculate offset after each server response.
+    #
+    # @param offset [Integer]
+    #   Identifier of the first update to be returned.
+    #   Must be greater by one than the highest among the identifiers of
+    #   previously received updates. By default, updates starting with the
+    #   earliest unconfirmed update are returned. An update is considered
+    #   confirmed as soon as getUpdates is called with an offset higher
+    #   than its update_id.
+    # @param limit [Integer]
+    #   Limits the number of updates to be retrieved. Values between 1—100 are accepted.
+    #   Defaults to 100
+    # @param timeout [Integer]
+    #   Timeout in seconds for long polling. Defaults to 0, i.e. usual short polling.
+    #
     # @return [Array<Telebot::Update>]
     def get_updates(offset: nil, limit: nil, timeout: nil)
       result = call(:getUpdates, offset: offset, limit: limit, timeout: timeout)
       result.map { |update_hash| Update.new(update_hash) }
     end
 
+    # Send text message.
+    #
+    # @param chat_id [Integer] Unique identifier for the message recipient - User or GroupChat id
+    # @param text [String] Text of the message to be sent
+    # @param disable_web_page_preview [Boolean] Disables link previews for links in this message
+    # @param reply_to_message_id [Integer] If the message is a reply, ID of the original message
+    # @param reply_markup [ReplyKeyboardMarkup, ReplyKeyboardHide, ForceReply] Additional interface options
+    #
     # @return [Telebot::Message]
     def send_message(chat_id:, text:, disable_web_page_preview: false, reply_to_message_id: nil, reply_markup: nil)
       result = call(:sendMessage,
@@ -57,15 +85,29 @@ module Telebot
       Message.new(result)
     end
 
+    # Use this method to forward messages of any kind.
+    #
+    # @param chat_id [Integer] Unique identifier for the message recipient - User or GroupChat id
+    # @param from_chat_id [Integer] Unique identifier for the chat where the original message was sent - User or GroupChat id
+    # @param message_id [Integer] Unique message identifier
+    #
+    # @return [Telebot::Message]
+    def forward_message(chat_id:, from_chat_id:, message_id:)
+      result = call(:forwardMessage, chat_id: chat_id, from_chat_id: from_chat_id, message_id: message_id)
+      Message.new(result)
+    end
+
+    # Send a picture.
+    #
     # @param chat_id [Integer] Unique identifier for the message recipient - User or GroupChat id
     # @param photo [InputFile, String] Photo to send. You can either pass a
     #   file_id as String to resend a photo that is already on the Telegram servers,
     #   or upload a new photo using multipart/form-data.
     # @param caption [String] Photo caption (may also be used when resending photos by file_id)
     # @param reply_to_message_id [Integer] If the message is a reply, ID of the original message
-    # @param reply_markup [ReplyKeyboardMarkup, ReplyKeyboardHide, ForceReply]
-    #   Additional interface options. A JSON-serialized object for a custom reply
-    #   keyboard, instructions to hide keyboard or to force a reply from the user.
+    # @param reply_markup [ReplyKeyboardMarkup, ReplyKeyboardHide, ForceReply] Additional interface options
+    #
+    # @return [Telebot::Message]
     def send_photo(chat_id:, photo:, caption: nil, reply_to_message_id: nil, reply_markup: nil)
       result = call(:sendPhoto,
         chat_id: chat_id,
@@ -77,10 +119,11 @@ module Telebot
       Message.new(result)
     end
 
+
+
     private def call(method_name, params = {})
       path = "/bot#{@token}/#{method_name}"
       faraday_response = @faraday.post(path, params)
-
       response = Response.new(faraday_response.body)
 
       if response.ok

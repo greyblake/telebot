3c63d42bb0c1f4b77437cfbbe4b8648d8e2a5987
Potapov Sergey <blake131313@gmail.com>
2015-07-12 23:17:56 +0200

    Implement addMe call

--- lib/telebot/client.rb
+++ lib/telebot/client.rb
@@ -1,10 +1,10 @@
 # Implemented:
+# * getMe
 # * getUpdates
 # * sendMessage
 # * sendPhoto
 #
 # REMAINING:
-# * getMe
 # * forwardMessage
 # * sendAudio
 # * sendDocument
@@ -28,17 +28,26 @@ module Telebot
         conn.response :json, :content_type => /\bjson$/
         conn.adapter adapter
       end
+    end
 
+    # A simple method for testing your bot's auth token. Requires no parameters.
+    # Returns basic information about the bot in form of a User object.
+    #
+    # @return [Telebot::User]
+    def get_me
+      result = call(:getMe)
+      User.new(result)
     end
 
     # @return [Array<Telebot::Update>]
     def get_updates(offset: nil, limit: nil, timeout: nil)
-      result = send_request(:getUpdates, offset: offset, limit: limit, timeout: timeout)
+      result = call(:getUpdates, offset: offset, limit: limit, timeout: timeout)
       result.map { |update_hash| Update.new(update_hash) }
     end
 
+    # @return [Telebot::Message]
     def send_message(chat_id:, text:, disable_web_page_preview: false, reply_to_message_id: nil, reply_markup: nil)
-      result = send_request(:sendMessage,
+      result = call(:sendMessage,
         chat_id: chat_id,
         text: text,
         disable_web_page_preview: disable_web_page_preview,
@@ -47,7 +56,6 @@ module Telebot
       )
       Message.new(result)
     end
-
 
     # @param chat_id [Integer] Unique identifier for the message recipient - User or GroupChat id
     # @param photo [InputFile, String] Photo to send. You can either pass a
@@ -59,7 +67,7 @@ module Telebot
     #   Additional interface options. A JSON-serialized object for a custom reply
     #   keyboard, instructions to hide keyboard or to force a reply from the user.
     def send_photo(chat_id:, photo:, caption: nil, reply_to_message_id: nil, reply_markup: nil)
-      result = send_request(:sendPhoto,
+      result = call(:sendPhoto,
         chat_id: chat_id,
         photo: photo,
         caption: caption,
@@ -69,7 +77,7 @@ module Telebot
       Message.new(result)
     end
 
-    private def send_request(method_name, params)
+    private def call(method_name, params = {})
       path = "/bot#{@token}/#{method_name}"
       faraday_response = @faraday.post(path, params)
 
